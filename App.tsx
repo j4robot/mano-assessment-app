8b3ca27a1192feb2d89a87f7400a6b5596502c4e
Emmanuel M. Achana <achanamosey@gmail.com>
2024-05-28 14:30:46 +0000

    Added the navigation for the screens designed

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+//Screens
+import { Product } from './src/screens/products';
+import { ProductDetails } from './src/screens/products/details';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Product' component={Product} options={{ headerShown: false }} />
+        <Stack.Screen name='ProductDetails' component={ProductDetails} options={
+          {
+            headerTitle: (props) => (<Text>Details</Text>),
+          }
+          } 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
